a896ad0e093ecb5831c1d49bd7f5aa5c57b53a5f
caroc24 <carolinacamarda@gmail.com>
2024-08-14 14:00:34 -0400

    added metadata

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div>
+      <meta name="description" content="The Little Lemon restaurant proudly serves mediterranean family recipes with a modern twist." />
+      <meta name="og:type" content="website"/>
+      <meta name="og:image" content="logo.jpg"/>
+      <meta name="og:url" content="https://littlelemon"/>
+      <meta name="og:title" content="Little Lemon Restaurant"/>
+      <meta name="og:description" content="Little Lemon is a family-owned Mediterranean restaurant that mixes family recipes with a modern twist."/>
+      <meta name="og:locale" content="en_US"/>
+      <meta name="og:site_name" content="Little Lemon"/>
       <h1>Homepage</h1>
+
     </div>
   );
 }
