851322ea3ec88b9a163f8a84a63f339fc684cc68
caroc24 <carolinacamarda@gmail.com>
2024-08-14 15:42:58 -0400

    added banner img

--- src/Components/CallToAction.js
+++ src/Components/CallToAction.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import banner from '../images/banner.jpg';
 
 function CallToAction(){
     return(
@@ -10,6 +11,9 @@ function CallToAction(){
                     <h2>Little Lemon</h2>
                     <h3>Chicago</h3>
                     <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                    <a href="/" className="banner-img">
+                        <img src ={banner} height={50} alt="Little Lemon logo"/>
+                    </a>
                     <button>Reserve a Table</button>
                 </div>
             </section>
