23153a60445babe755405706555320c7974d5d44
caroc24 <carolinacamarda@gmail.com>
2024-08-14 14:34:17 -0400

    updated navbar css

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../App.css';
 import logo from '../images/logo.jpg';
 
 const Nav = () => {
     return <nav className="navbar">
-        <a href="/" classname="logo">
+        <a href="/" className="logo">
             <img src ={logo} height={50} alt="Little Lemon logo"/>
         </a>
 
