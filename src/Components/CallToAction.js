00477a9454ab6f8f6a4b11cd20984d9691dfd4db
caroc24 <carolinacamarda@gmail.com>
2024-08-14 15:03:22 -0400

    created the call to action for the homepage

---
+++ src/Components/CallToAction.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+const CallToAction = () => {
+    return(
+        <header className='header'>
+            <section>
+                /* Banner Texts */
+                <div>
+                    <h2>Little Lemon</h2>
+                    <h3>Chicago</h3>
+                </div>
+            </section>
+        </header>
+    )
+}
+export default CallToAction;
