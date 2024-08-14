edd49d280821fa1f896b9f1ef19204151940c6c7
caroc24 <carolinacamarda@gmail.com>
2024-08-14 17:20:48 -0400

    updated call to action

--- src/Components/CallToAction.js
+++ src/Components/CallToAction.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import '../App.css';
 import banner from '../images/banner.jpg';
-import Reservations from './Reservations';
-import {Routes, Route} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function CallToAction(){
     return(
-        <header className='header'>
+        <header className="header">
             <section>
-                {/* Banner Texts */}
-                <div className='banner'>
+                <div className="banner">
                     <h2>Little Lemon</h2>
                     <h3>Chicago</h3>
-                    <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-                    <a href="/" className="banner-img">
-                        <img src ={banner} height={50} alt="Little Lemon logo"/>
-                    </a>
-                    <Routes>
-                        <Route path="/reservations" element={<Reservations />} />
-                    </Routes>
-
-                    <button>Reserve a Table</button>
+                    <p>
+                    We are a family owned Mediterraneran restaurant, focused on
+                    traditional recipes servred with a modern twist.
+                    </p>
+                    <Link to="/booking">
+                    <button aria-label="On Click">Reserve Table</button>
+                    </Link>
+                </div>
+                <div className="banner-img">
+                    <img src={banner} alt="" />
                 </div>
             </section>
         </header>
